2f94954e1ed7c9edcf87987cc4fc3534aa6762fb
Luca Martinelli <lucxmangajet@gmail.com>
2020-03-08 21:18:19 +0100

    fixed compile error

--- Source/CoopLearning/Private/SGameModeBase.cpp
+++ Source/CoopLearning/Private/SGameModeBase.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "SPlayerState.h"
-#include "SGameModeBase.h"
 #include "SGameStateBase.h"
+#include "SGameModeBase.h"
+
 
 void ASGameModeBase::ResetAllKDA()
 {
